7d34bcfe4c79f5dac297ee9a830c73a464856f03
duykhanh.nguyen <nduykhanh109@gmail.com>
2017-05-02 18:57:52 +0700

    update

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
     renderList(){
         return this.props.books.map((book) =>{
             return(
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li
+                    onClick={() => this.props.selectBook(book)}
+                    key={book.title} className="list-group-item">{book.title}</li>
             );
         })
     }
@@ -25,4 +29,8 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({selectBook:selectBook}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function(){
     return [
-        {title: 'Title book 1'},
-        {title: 'Title book 2'},
-        {title: 'Title book 3'},
-        {title: 'Title book 4'},
-        {title: 'Title book 5'}
+        {title: 'Title book 1', pages: 10},
+        {title: 'Title book 2', pages: 2},
+        {title: 'Title book 3', pages: 5},
+        {title: 'Title book 4', pages: 1},
+        {title: 'Title book 5', pages: 2}
     ];
 }
