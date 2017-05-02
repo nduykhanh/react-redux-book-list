2e3c9a3627b07340503e6a832e94ac7f3409d0c5
duykhanh.nguyen <nduykhanh109@gmail.com>
2017-05-02 18:12:51 +0700

    update first

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return [
+        {title: 'Title book 1'},
+        {title: 'Title book 2'},
+        {title: 'Title book 3'},
+        {title: 'Title book 4'},
+        {title: 'Title book 5'}
+    ];
+}
